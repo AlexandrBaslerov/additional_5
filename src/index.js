aa5cdd16b3307d2b5b35435aeb1d51c4ce402b90
Alexander Baslerov <basson-777@yandex.ru>
2017-09-29 23:55:12 +0300

    additional_5\done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, bracketsConfig) {
+    var x = s.length+1;
+    while(s = s.replace(/\<\>|\[\]|\{\}|\(\)|\|\||12|34|56|77|88/,'')){
+        if(s.length==x && x!=0)return false;
+        if(x==0)return true;
+        x=s.length;
+    }
+    return true;
 }
